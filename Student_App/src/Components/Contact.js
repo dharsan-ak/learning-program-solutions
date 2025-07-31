8c38129f8684c165e10ad4600843be79b9bc03d7
dharsan-ak <dharsan.ak.ece.2022@snsce.ac.in>
2025-07-31 22:07:04 +0530

    Delete Student_App directory

--- Student_App/src/Components/Contact.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Contact() {
-  return <h2>Welcome to the Contact Page of Student Management Portal</h2>;
-}
-
-export default Contact;
--- Student_App/src/Components/Home.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Home() {
-  return <h2>Welcome to the Home Page of Student Management Portal</h2>;
-}
-
-export default Home;
